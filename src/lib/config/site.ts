5757640541d6e597d4ff722db23b8f4648a95b8e
areenkun <101259369+areenkun@users.noreply.github.com>
2024-05-09 13:03:10 +0700

    Update site.ts

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -2,7 +2,7 @@ import type { SiteConfig } from '$lib/types/site'
 
 export const site: SiteConfig = {
   protocol: import.meta.env.URARA_SITE_PROTOCOL ?? import.meta.env.DEV ? 'http://' : 'https://',
-  domain: import.meta.env.URARA_SITE_DOMAIN ?? 'wimalsen.pages.dev',
+  domain: import.meta.env.URARA_SITE_DOMAIN ?? 'wimalsen.is-cool.dev',
   title: 'Wimalsen',
   subtitle: 'Asistensi Mengajar MTs Almaarif 01 Singosari',
   lang: 'en-US',
