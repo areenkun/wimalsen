4577d0e78c7e3d901b70b9af889bd19e8919cee5
areen-c <74430814+areen-c@users.noreply.github.com>
2024-05-09 14:47:17 +0700

    Update site.ts

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -4,7 +4,7 @@ export const site: SiteConfig = {
   protocol: import.meta.env.URARA_SITE_PROTOCOL ?? import.meta.env.DEV ? 'http://' : 'https://',
   domain: import.meta.env.URARA_SITE_DOMAIN ?? 'wimalsen.is-cool.dev',
   title: 'Wimalsen',
-  subtitle: 'Asistensi Mengajar MTs Almaarif 01 Singosari',
+  subtitle: 'Teaching Assistantship of MTs Almaarif 01 Singosari',
   lang: 'en-US',
   description: 'Asistensi Mengajar MTs Almaarif 01 Singosari',
   author: {
